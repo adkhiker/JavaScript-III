555f7d1c3418e14ac3770cc750e9fc6c905ccef3
adkhiker <poirier.greg@gmail.com>
2019-06-12 15:55:04 -0400

    WIP: working on this

--- assignments/this.js
+++ assignments/this.js
@@ -1,21 +1,33 @@
 /* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
-*
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
-* write out a code example of each explanation above
-*/
+ * in your own words. explain the four principle for the "this" keyword below.
+ *
+ * 1.
+ * 2.
+ * 3.
+ * 4.
+ *
+ * write out a code example of each explanation above
+ */
 
 // Principle 1
-
 // code example for Window Binding
+function sayHello(string) {
+  console.log(this);
+  return string;
+}
+sayHello("Hello");
 
 // Principle 2
-
 // code example for Implicit Binding
+var whoAmI = {
+  name: 'Greg',
+  sex: 'male',
+  sayName: function() {
+    console.log(this.name);
+  }
+};
+
+whoAmI.sayName();
 
 // Principle 3
 
