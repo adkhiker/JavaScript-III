b798752461ebef0e4d5319ed5e4e5f5070e62c3f
adkhiker <poirier.greg@gmail.com>
2019-06-12 16:11:54 -0400

    WIP:  this in progress

--- assignments/this.js
+++ assignments/this.js
@@ -20,19 +20,29 @@ sayHello("Hello");
 // Principle 2
 // code example for Implicit Binding
 var whoAmI = {
-  name: 'Greg',
-  sex: 'male',
+  name: "Greg",
+  sex: "male",
   sayName: function() {
     console.log(this.name);
   }
 };
-
 whoAmI.sayName();
 
 // Principle 3
-
 // code example for New Binding
+function placeOrder(item) {
+  this.Item = item;
+  this.food = function() {
+    console.log(
+      `I am hungry for ${this.Item} today. Think I'll order lunch now.`
+    );
+  };
+}
+const pepperoniPizza = new placeOrder("pepperoni pizza");
+const chineseFood = new placeOrder("Kung Poo");
+
+chineseFood.food();
+pepperoniPizza.food();
 
 // Principle 4
-
 // code example for Explicit Binding
